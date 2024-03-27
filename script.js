925f3ee9eb52277c7a8e51cf6446d1a1d808c55b
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-03-27 10:13:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
-  // your code here
+  if (!s || s.indexOf(' ') === -1) {
+    return s;
+  } else {
+    return s.substring(0, s.indexOf(' '));
+  }
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// Test cases
+console.log(firstWord('see and stop'));   // Output: 'see'
+console.log(firstWord(' Hello World!'));  // Output: 'Hello'
+console.log(firstWord('12345'));          // Output: '12345'
+console.log(firstWord(''));               // Output: ''
